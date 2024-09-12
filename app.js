095536764ffad0cbb26b9ef5712559a418485a86
GianBorlenghi <gianclas@gmail.com>
2024-09-12 10:15:19 -0300

    2D

--- app.js
+++ app.js
@@ -1,70 +1,234 @@
 emailjs.init("rt1v_cA4avrTQRGue"); 
 
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Credenciales estáticas (puedes modificar esto para usar credenciales dinámicas)
+    /*const validUsername = 'admin';
+    const validPassword = '1234';*/
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    buscarData(username).then(resultado => {
+        let existeUsuario = resultado; // Ahora ya tienes el valor de buscarData
+    
+        if(existeUsuario === null){
+            console.log("Usuario no encontrado");
+            document.getElementById('loginError').classList.remove('hidden');
+        } else if (existeUsuario !== undefined && password == existeUsuario.pw) {
+            // Supongo que existeUsuario tiene una propiedad 'password' que quieres comparar
+            // Guardar el estado de login en localStorage
+            localStorage.setItem('isLoggedIn', 'true');
+            localStorage.setItem('mail',existeUsuario.mail)
+            showQuiz();
+        } else {
+            console.log("Contraseña incorrecta");
+            document.getElementById('loginError').classList.remove('hidden');
+        }
+    }).catch(error => {
+        console.error('Error al buscar el usuario:', error);
+    });
+    
+  
+});
+
+function checkLoginStatus() {
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+    if (isLoggedIn === 'true') {
+        showQuiz();
+    } else {
+        showLogin();
+    }
+}
+
+function showQuiz() {
+    document.getElementById('loginContainer').classList.add('hidden');
+    document.getElementById('quizContent').classList.remove('hidden');
+    checkIfAlreadyAnswered();
+
+}
+
+function showLogin() {
+    document.getElementById('loginContainer').classList.remove('hidden');
+    document.getElementById('quizContent').classList.add('hidden');
+}
+
+function obtenerUsuario() {
+    buscarData(username).then(resultado => {
+        return resultado; // Aquí ya tienes el valor
+        // Puedes hacer algo con el resultado aquí
+    });
+}
+
 // Array de preguntas y respuestas
 const questions = [
     {
-        question: "¿Cuál es el lenguaje principal de la web?",
-        options: ["Python", "HTML", "Java", "C++"],
-        answer: "HTML",
+        question: "¿Qué es una computadora?",
+        options: ["Un buscador de información", "Un dispositivo electronico capaz de procesar y almacenar información", "Un dispositivo con circuitos integrados"],
+        answer: "Un dispositivo electronico capaz de procesar y almacenar información",
         type: "multiple" // Tipo de pregunta
     },
     {
-        question: "¿Qué significa CSS?",
-        options: ["Color Style Sheets", "Cascading Style Sheets", "Computing Style Sheets", "Creative Style Sheets"],
-        answer: "Cascading Style Sheets",
+        question: "¿Que simbolo se utiliza en EXCEL para realizar una formula o una operación?",
+        options: ["=", "/", "*", "+"],
+        answer: "=",
         type: "multiple"
     },
     {
-        question: "¿En qué año se lanzó JavaScript?",
-        options: ["1995", "1996", "1997", "1998"],
-        answer: "1995",
+        question: "¿Que empresa creó Excel?",
+        options: ["Microsoft", "Apple", "HP"],
+        answer: "Microsoft",
         type: "multiple"
     },
     {
-        question: "¿Qué es un algoritmo?",
-        options: ["Un tipo de dato", "Un conjunto de instrucciones", "Un lenguaje de programación", "Un error en el código"],
-        answer: "Un conjunto de instrucciones",
+        question: "¿Qué es el hardware de una computadora?",
+        options: ["Un tipo de dato", "Toda la parte fisica de la computadora", "La parte lógica", "Un error"],
+        answer: "Toda la parte fisica de la computadora",
         type: "multiple"
     },
     {
-        question: "¿Cuál es el sistema operativo más utilizado en servidores web?",
-        options: ["Linux", "Windows", "MacOS", "Unix"],
-        answer: "Linux",
+        question: "¿Cuál de los siguientes pertenece al hardware de la computadora?",
+        options: ["Google Chrome", "Windows", "Memoria RAM", "iOS"],
+        answer: "Memoria RAM",
         type: "multiple"
     },
     {
-        question: "Escribe el nombre del lenguaje utilizado para crear aplicaciones móviles nativas en Android:",
-        answers: ["Kotlin", "kotlin", "Es Kotlin", "es kotlin"], // Varias respuestas válidas
-        type: "text" // Tipo de pregunta
+        question: "¿Que tipo de software es Windows?",
+        options: ["Aplicación", "Sistema", "Programación", "Gestión"], // Varias respuestas válidas
+        answer:"Sistema",
+        type: "multiple" // Tipo de pregunta
     },
     {
-        question: "¿Qué significa HTTP?",
-        options: ["HyperText Transfer Protocol", "HyperText Transfer Program", "High Transfer Text Protocol", "HyperText Technical Protocol"],
-        answer: "HyperText Transfer Protocol",
+        question: "¿Que es un periferico?",
+        options: ["Es un dispositivo auxiliar e independiente de la computadora que se conecta a la placa base.", "Es un sistema operativo"],
+        answer: "Es un dispositivo auxiliar e independiente de la computadora que se conecta a la placa base.",
         type: "multiple"
     },
     {
-        question: "¿Qué es una API?",
-        options: ["Advanced Protocol Interface", "Application Programming Interface", "Android Programming Interface", "Access Protocol Interface"],
-        answer: "Application Programming Interface",
+        question: "¿Qué función cumplen los periféricos de entrada?",
+        options: ["Muestran o proyectan información hacia el exterior del ordenador.", "Permiten introducir datos o información en una computadora para que los procese."],
+        answer: "Permiten introducir datos o información en una computadora para que los procese.",
         type: "multiple"
     },
     {
-        question: "¿Qué framework de JavaScript es mantenido por Facebook?",
-        options: ["Angular", "Vue", "React", "Ember"],
-        answer: "React",
+        question: "¿Qué función cumplen los periféricos de salida?",
+        options: ["Permiten introducir datos o información en una computadora para que los procese.", "Muestran o proyectan información hacia el exterior del ordenador."],
+        answer: "Muestran o proyectan información hacia el exterior del ordenador.",
         type: "multiple"
     },
     {
-        question: "¿Qué lenguaje se utiliza para consultas en bases de datos relacionales?",
-        options: ["NoSQL", "HTML", "CSS", "SQL"],
-        answer: "SQL",
+        question: "El sistema binario utiliza:",
+        options: ["Un número", "Dos números", "Diez números"],
+        answer: "Dos números",
+        type: "multiple"
+    },
+    {
+        question: "El sistema binario es utilizado por la computadora y todos los dispositivos electrónicos",
+        options: ["Verdadero","Falso"],
+        answer: "Verdadero",
+        type: "multiple"
+    },
+    {
+        question: "La unidad mas básica de información es",
+        options: ["El bit","El byte"],
+        answer: "El bit",
+        type: "multiple"
+    },
+    {
+        question: "1 byte equivale a ",
+        options: ["2 bits","6 bits","8 bits"],
+        answer: "8 bits",
+        type: "multiple"
+    },
+    {
+        question: "El sistema binario es un sistema en base dos.",
+        options: ["Verdadero","Falso"],
+        answer: "Verdadero",
+        type: "multiple"
+    },
+    {
+        question: "El atajo de teclado CTRL + C sirve para:",
+        options: ["Copiar","Pegar","Cortar","Borrar"],
+        answer: "Copiar",
+        type: "multiple"
+    },
+    {
+        question: "El atajo de teclado CTRL + X sirve para:",
+        options: ["Copiar","Pegar","Cortar","Borrar"],
+        answer: "Cortar",
+        type: "multiple"
+    },
+    {
+        question: "El atajo de teclado CTRL + V sirve para:",
+        options: ["Copiar","Pegar","Cortar","Borrar"],
+        answer: "Pegar",
+        type: "multiple"
+    },
+    {
+        question: "El atajo de teclado CTRL + Z sirve para:",
+        options: ["Copiar","Pegar","Cortar","Borrar","Deshacer"],
+        answer: "Deshacer",
+        type: "multiple"
+    },
+    {
+        question: "¿Qué extensión tienen los libros de Excel?",
+        options: [".DOC",".XLS",".JPEG",".TXT"],
+        answer: ".XLS",
+        type: "multiple"
+    },
+    {
+        question: "La hoja de cálculo de Excel, esta definida por:",
+        options: ["Filas","Columnas","Celdas","Columnas y filas","Filas, columnas y celdas"],
+        answer: "Filas, columnas y celdas",
+        type: "multiple"
+    },
+    {
+        question: "Supongamos que quiero sumar la celda A5 y la celda D2 en la celda E1, ¿Que debo escribir?",
+        answers: ["=A5+D2","=a5+d2","=d2+a5","=D2+A5"],
+        type: "text"
+    },
+    {
+        question: "El rango en Excel es:",
+        options: ["Una celda","La selección de celdas contiguas"],
+        answer: "La selección de celdas contiguas",
+        type: "multiple"
+    },
+    {
+        question: "B2:C9 es un ejemplo de rango en Excel",
+        options: ["Verdadero","Falso"],
+        answer: "Verdadero",
+        type: "multiple"
+    },
+    {
+        question: "¿Cual de los siguientes símbolos, es un operador aritmético?",
+        options: ["$","&","¬","*"],
+        answer: "*",
+        type: "multiple"
+    },
+    {
+        question: "La placa base, ¿forma parte del software o del hardware?",
+        options: ["Software","Hardware"],
+        answer: "Hardware",
+        type: "multiple"
+    },
+    {
+        question: "Microsoft Excel, ¿es un software?",
+        options: ["Verdadero","Falso"],
+        answer: "Verdadero",
+        type: "multiple"
+    },
+    {
+        question: "¿Microsoft PowerPoint, Microsoft Word son parte del paquete office?",
+        options: ["Verdadero","Falso"],
+        answer: "Verdadero",
         type: "multiple"
     }
 ];
 
 // Seleccionar 5 preguntas aleatorias
-const selectedQuestions = questions.sort(() => 0.5 - Math.random()).slice(0, 5);
+const selectedQuestions = questions.sort(() => 0.5 - Math.random()).slice(0, 10);
 
 const quizContainer = document.getElementById('quizContainer');
 const quizForm = document.getElementById('quizForm');
@@ -189,7 +353,7 @@ quizForm.addEventListener('submit', (e) => {
             const correctAnswers = q.answers.map(a => a.toLowerCase());
             if (correctAnswers.includes(textAnswer)) {
                 score++;
-                resultDiv.innerHTML = `<strong>${index + 1}. ${q.question}</strong> - <span class="correct">Correcto</span>`;
+                resultDiv.innerHTML = `<strong>${index + 1}. ${q.question}</strong> - <span class="correct">Correcto</span> | <strong>${textAnswer}</strong>`;
             } else {
                 resultDiv.innerHTML = `<strong>${index + 1}. ${q.question}</strong> - <span class="incorrect">Incorrecto</span>. Respuesta correcta: <strong>${q.answers.join(', ')}</strong>`;
             }
@@ -250,3 +414,18 @@ console.log($(mail).val())
 
 });
 
+
+
+   
+//onsole.log(buscarData("Ana"))
+async function buscarData(nombreUsuario) {
+    try {
+        const response = await fetch('datos.json');
+        const data = await response.json();
+        
+        const obj = data.find(item => item.usuario === nombreUsuario);
+        return obj; // Ahora devolverá el objeto correcto
+    } catch (error) {
+        console.error('Error al leer el archivo JSON:', error);
+    }
+}
